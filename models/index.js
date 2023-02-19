22a48ab8c76ddfc58dfb431e82384ff3c521c93e
mrgookis <124477733+mrgookis@users.noreply.github.com>
2023-02-19 07:34:06 -0700

    creat action move todo

--- models/index.js
+++ models/index.js
@@ -1,17 +1,3 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-
-
-const TodoSchema = new Schema( {
-title: {type: String, required: true},
-author: {type: String, required: true},
-body: {type: String},
-createdAt: {type: Date, default: Date.now()},
-updatedAt: {type: Date},
-isComplete: {type: Boolean},
-})
-
-const Todo = mongoose.model('Todo',TodoSchema);
-
-moduel.exports = Todo;
+moduel.exports = {
+    Todo: require('./todo'),
+}
--- src/action.js
+++ src/action.js
@@ -1,3 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const db = require('./models')
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb:localhost/todo');
+
+async function seed() {
+    const seedData = [
+        {title: 'cron action', body: 'cron job', isComplete: true},
+        {title: 'cron action - 2', body: 'cron job - 2', isComplete: true}
+    ]
+    db.Todo.collection.insertMany(seedData).then(data => {
+        console.log(`${data.result.n} records inserted!`)
+    }).catch(err => console.log(err));
+}
+
+seed()
