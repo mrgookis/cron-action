9dd9d3bc0ebd4486a816d0d703bd2cf00bf31e8c
mrgookis <124477733+mrgookis@users.noreply.github.com>
2023-02-20 14:20:58 +0000

    fix: action

--- src/action.js
+++ src/action.js
@@ -1,19 +1,33 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const db = require('../models');
-dotenv.config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo');
+dotenv.config();
 
-async function seed() {
-    const seedData = [
-        {title: 'cron action', body: 'cron job', isComplete: true},
-        {title: 'cron action - 2', body: 'cron job - 2', isComplete: true}
-    ]
-    db.Todo.collection.insertMany(seedData).then(data => {
-        console.log(`${data.result.n} records inserted!`)
-    }).catch(err => console.log(err));
-    // db.Todo.find({}).then(data =>console.log(data.data))
-}
+mongoose.set('strictQuery',false)
 
-seed()
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', async () => {
+  console.log('Connected to MongoDB');
+
+  const seedData = [
+    {title: 'cron action', body: 'cron job', isComplete: true},
+    {title: 'cron action - 2', body: 'cron job - 2', isComplete: true}
+  ];
+
+  try {
+    const result = await db.Todo.collection.insertMany(seedData);
+    console.log(`${result.result.length} records inserted!`);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit();
+  }
+});
